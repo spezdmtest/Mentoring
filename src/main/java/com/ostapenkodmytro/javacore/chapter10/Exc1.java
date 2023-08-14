fc6820c6bfef151f4f000592164dcca49cc59bd3
spezdmtest <spezd.m.@gmail.com>
2023-08-14 17:30:27 +0300

    Exception

---
+++ src/main/java/com/ostapenkodmytro/javacore/chapter10/Exc1.java
@@ -0,0 +1,12 @@
+package com.ostapenkodmytro.javacore.chapter10;
+
+public class Exc1 {
+    static void subrouting() {
+        int d = 0;
+        int a = 10 / d;
+    }
+
+    public static void main(String[] args) {
+        Exc1.subrouting();
+    }
+}
