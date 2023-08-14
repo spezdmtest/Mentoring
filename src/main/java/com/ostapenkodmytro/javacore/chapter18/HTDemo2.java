8094d5177fc549f6924fdf52694008f5fbe75a92
spezdmtest <spezd.m.@gmail.com>
2023-08-14 17:26:34 +0300

    Collection

---
+++ src/main/java/com/ostapenkodmytro/javacore/chapter18/HTDemo2.java
@@ -0,0 +1,37 @@
+package com.ostapenkodmytro.javacore.chapter18;
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Set;
+
+public class HTDemo2 {
+    public static void main(String[] args) {
+        Hashtable<String, Double> balance = new Hashtable<>();
+        Enumeration<String> names;
+
+        String str;
+        double bal;
+
+        balance.put("Джон Доу", 3434.34);
+        balance.put("Том Смит", 123.22);
+        balance.put("Джейн Бейкер", 1378.00);
+        balance.put("Тод Холл", 99.22);
+        balance.put("Ральф Смит", -19.08);
+
+        Set<String> set = balance.keySet();
+
+        Iterator<String> itr = set.iterator();
+
+        while (itr.hasNext()) {
+            str = itr.next();
+            System.out.println(str + ": " + balance.get(str));
+        }
+
+        System.out.println();
+
+        bal = balance.get("Джон Доу");
+        balance.put("Джон Доу", bal + 1000);
+        System.out.println("Новый остаток на счете Джона Доу: " + balance.get("Джон Доу"));
+    }
+}
