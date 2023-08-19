198f55d7e4b4f373937d453756b78c2338b9259e
spezdmtest <spezd.m.@gmail.com>
2023-08-19 10:06:39 +0300

    Lambda

--- src/main/java/com/ostapenkodmytro/javacore/chapter15/VarCapture.java
+++ src/main/java/com/ostapenkodmytro/javacore/chapter15/VarCapture.java
@@ -8,9 +8,8 @@ public class VarCapture {
     public static void main(String[] args) {
         int num = 10;
 
-        MyFunc myLambda = (n) -> {
-            return num + n;
-        };
+        MyFunc myLambda = (n) -> num + n;
+
         System.out.println(myLambda.func(0));
     }
 }
