c44f6c81c533608a01563e13d8bf2b2efa2e3c45
spezdmtest <spezd.m.@gmail.com>
2023-08-30 10:50:29 +0300

    CRUD API

--- AppCrud/src/main/java/com/ostapenkodmytro/javacore/Main.java
+++ AppCrud/src/main/java/com/ostapenkodmytro/javacore/Main.java
@@ -1,36 +1,10 @@
 package com.ostapenkodmytro.javacore;
 
-import com.ostapenkodmytro.javacore.controller.PostController;
-import com.ostapenkodmytro.javacore.controller.WriterController;
-import com.ostapenkodmytro.javacore.repository.gson.GsonPostRepositoryImpl;
-import com.ostapenkodmytro.javacore.repository.gson.GsonWriterRepositoryImpl;
-import com.ostapenkodmytro.javacore.view.WriterView;
-
 /**
  * Crud API!
  */
 public class Main {
     public static void main(String[] args) {
-
-//        GsonLabelRepositoryImpl gsonLabelRepository = new GsonLabelRepositoryImpl();
-//        LabelController labelController = new LabelController(gsonLabelRepository);
-//        LabelView labelView = new LabelView(labelController);
-//        labelView.run();
-
-//        GsonLabelRepositoryImpl gsonLabelRepository = new GsonLabelRepositoryImpl();
-//        GsonPostRepositoryImpl gsonPostRepository = new GsonPostRepositoryImpl();
-//        LabelController labelController = new LabelController(gsonLabelRepository);
-//        PostController postController = new PostController(gsonPostRepository);
-//        PostView postView = new PostView(postController,labelController);
-//        postView.run();
-//
-//        GsonPostRepositoryImpl gsonPostRepository = new GsonPostRepositoryImpl();
-//        GsonWriterRepositoryImpl gsonWriterRepository = new GsonWriterRepositoryImpl();
-//        PostController postController = new PostController(gsonPostRepository);
-//        WriterController writerController = new WriterController(gsonWriterRepository);
-//        WriterView writerView = new WriterView(postController, writerController);
-//        writerView.run();
-
         ApplicationContext applicationContext = new ApplicationContext();
         applicationContext.start();
     }
