1ee38617af18c2221c13b15c1aefac0df90f16a5
spezdm <spez.dm@gmail.com>
2023-09-07 12:03:35 +0300

    Example module 1_4

---
+++ src/main/java/com/ostapenkodmytro/javacore/Example1_4/FooDemo.java
@@ -0,0 +1,74 @@
+package com.ostapenkodmytro.javacore.Example1_4;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Semaphore;
+
+public class FooDemo {
+    public static void main(String[] args) throws InterruptedException {
+        Foo foo = new Foo();
+        CompletableFuture.runAsync(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    foo.first(this);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        CompletableFuture.runAsync(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    foo.second(this);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        CompletableFuture.runAsync(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    foo.third(this);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+    }
+}
+
+class Foo {
+    private Semaphore semaphore1;
+    private Semaphore semaphore2;
+
+    public Foo() {
+        semaphore1 = new Semaphore(1);
+        semaphore2 = new Semaphore(1);
+        try {
+            semaphore1.acquire();
+            semaphore2.acquire();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    void first(Runnable r) throws InterruptedException {
+        print("first");
+        semaphore1.release();
+    }
+    void second(Runnable r) throws InterruptedException {
+        semaphore1.acquire();
+        print("second");
+        semaphore2.release();
+    }
+    void third(Runnable r) throws InterruptedException {
+        semaphore2.acquire();
+        print("third");
+    }
+    private void print(String str) {
+        System.out.print(str);
+    }
+}
