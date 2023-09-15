4ecf910dc47dd6a98fb8de2f723fb2526dfd3ab0
spezdm <spez.dm@gmail.com>
2023-09-15 11:57:06 +0300

    Pattern Prototype

---
+++ src/main/java/com/ostapenkodmytro/javacore/basepatterns/creational/prototype/Copyable.java
@@ -0,0 +1,5 @@
+package com.ostapenkodmytro.javacore.basepatterns.creational.prototype;
+
+public interface Copyable {
+    Object copy();
+}
---
+++ src/main/java/com/ostapenkodmytro/javacore/basepatterns/creational/prototype/ProjectFactory.java
@@ -0,0 +1,18 @@
+package com.ostapenkodmytro.javacore.basepatterns.creational.prototype;
+
+public class ProjectFactory {
+    Project project;
+
+    public ProjectFactory(Project project) {
+        this.project = project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    Project cloneProject() {
+        return (Project) project.copy();
+    }
+
+}
