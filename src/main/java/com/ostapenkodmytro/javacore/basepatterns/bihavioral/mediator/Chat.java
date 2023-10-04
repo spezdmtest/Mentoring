2d43714faae05e8bc4ff3b0dd3a568a78792cba0
spezdmtest <spezd.m.@gmail.com>
2023-10-04 10:49:23 +0300

    Pattern Mediator

---
+++ src/main/java/com/ostapenkodmytro/javacore/basepatterns/bihavioral/mediator/Chat.java
@@ -0,0 +1,5 @@
+package com.ostapenkodmytro.javacore.basepatterns.bihavioral.mediator;
+
+public interface Chat {
+    void sendMessage(String message, User user);
+}
---
+++ src/main/java/com/ostapenkodmytro/javacore/basepatterns/bihavioral/mediator/User.java
@@ -0,0 +1,7 @@
+package com.ostapenkodmytro.javacore.basepatterns.bihavioral.mediator;
+
+public interface User {
+    void sendMessage(String message);
+
+    void getMessage(String message);
+}
