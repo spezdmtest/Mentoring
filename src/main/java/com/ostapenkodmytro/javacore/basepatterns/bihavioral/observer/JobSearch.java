21be5d9f09573b6e1db037df1cdd282c9e5c3079
spezdmtest <spezd.m.@gmail.com>
2023-10-06 10:49:09 +0300

    Pattern Observer

---
+++ src/main/java/com/ostapenkodmytro/javacore/basepatterns/bihavioral/observer/JobSearch.java
@@ -0,0 +1,19 @@
+package com.ostapenkodmytro.javacore.basepatterns.bihavioral.observer;
+
+public class JobSearch {
+    public static void main(String[] args) {
+        JavaDeveloperJobSite jobSite = new JavaDeveloperJobSite();
+        jobSite.addVacancy("First Java Position");
+        jobSite.addVacancy("Second Java Position");
+
+        Observer firstSubscriber = new Subscriber("Dmytro Ostapenko");
+        Observer secondSubscriber = new Subscriber("Peter Romanenko");
+
+        jobSite.addObserver(firstSubscriber);
+        jobSite.addObserver(secondSubscriber);
+
+        jobSite.addVacancy("Third Java Position");
+
+        jobSite.removeVacancy("First Java Position");
+    }
+}
---
+++ src/main/java/com/ostapenkodmytro/javacore/basepatterns/bihavioral/observer/Subscriber.java
@@ -0,0 +1,17 @@
+package com.ostapenkodmytro.javacore.basepatterns.bihavioral.observer;
+
+import java.util.List;
+
+public class Subscriber implements Observer {
+    String name;
+
+    public Subscriber(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void handleEvent(List<String> vacancies) {
+        System.out.println("Dear " + name + "\n We have some changes in vacancies:\n" + vacancies +
+                "\n===============================================================================\n");
+    }
+}
