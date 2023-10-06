8a9047d140bd3ffbbfdce3785e995fcee6a1136d
spezdmtest <spezd.m.@gmail.com>
2023-10-06 10:48:24 +0300

    Pattern Momento

---
+++ src/main/java/com/ostapenkodmytro/javacore/basepatterns/bihavioral/momento/GitHubRepo.java
@@ -0,0 +1,13 @@
+package com.ostapenkodmytro.javacore.basepatterns.bihavioral.momento;
+
+public class GitHubRepo {
+    private Save save;
+
+    public Save getSave() {
+        return save;
+    }
+
+    public void setSave(Save save) {
+        this.save = save;
+    }
+}
